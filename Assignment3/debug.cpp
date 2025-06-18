fad9845d236ff8b1583ae6af8e23652e5b254ace
bob <AustinBraun007@gmail.com>
2025-06-18 14:58:19 -0700

    made debug.cpp print on new line

--- Assignment3/debug.cpp
+++ Assignment3/debug.cpp
@@ -9,13 +9,13 @@ int main() {
     std::cin >> n1 >> n2 >> n3;
 
     if(n1 >= n2 && n1 >= n3) {
-        cout << "Largest number: " << n1;
+        cout << "Largest number: " << n1 << std::endl;
     }
     else if(n2 >= n1 && n2 >= n3) {
-        cout << "Largest number: " << n2;
+        cout << "Largest number: " << n2 << std::endl;
     }
     else if(n3 >= n1 && n3 >= n2) {
-        cout << "Largest number: " << n3;
+        cout << "Largest number: " << n3 << std::endl;
     }
     return 0;
 }
