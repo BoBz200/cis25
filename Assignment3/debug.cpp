78ff9cf51c6f5b065f3c07411b52fc6117159306
bob <AustinBraun007@gmail.com>
2025-06-18 14:53:47 -0700

    fixed debug.cpp in assignment 3

--- Assignment3/debug.cpp
+++ Assignment3/debug.cpp
@@ -11,10 +11,10 @@ int main() {
     if(n1 >= n2 && n1 >= n3) {
         cout << "Largest number: " << n1;
     }
-    if(n2 >= n1 && n2 >= n3) {
+    else if(n2 >= n1 && n2 >= n3) {
         cout << "Largest number: " << n2;
     }
-    if(n3 >= n1 && n3 >= n2) {
+    else if(n3 >= n1 && n3 >= n2) {
         cout << "Largest number: " << n3;
     }
     return 0;
